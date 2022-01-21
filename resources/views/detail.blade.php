48db9a43ce6ba225fe89d2bfd118cdf02f539db9
Bantasolagratia <85103735+Bantasolagratia@users.noreply.github.com>
2022-01-21 20:55:33 +0700

    eperpus

---
+++ resources/views/detail.blade.php
@@ -0,0 +1,96 @@
+<html>
+<head>
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>E-Perpus</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <script defer src="https://use.fontawesome.com/releases/v5.1.0/js/all.js" integrity="sha384-3LK/3kTpDE/Pkp8gTNp2gR/2gOiwQ6QaO7Td0zV76UFJVhqLl4Vl3KL1We6q6wR9" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <link href="../css/app.css" rel="stylesheet">
+  
+</head>
+<body>
+  <div class="wrapper">
+
+    <!-- HEADER -->
+    <header>
+      <div class="netflixLogo">
+        <a id="logo" href="#home"><img src="../image/logo.png" alt="Logo Image"></a>
+      </div>      
+      <nav class="main-nav">                
+       <div class="search">
+        <input type="search" id="search" class="form-control" placeholder="Search" >
+        <button type="SUBMIT" class="btn btn-primary">
+          <i class="fas fa-search"></i>
+        </button>
+      </div>
+      </nav>
+      <nav class="sub-nav">
+        <a href="#">Bantuan</a>
+       
+        <a href="#"><i class="fas fa-bell sub-nav-logo"></i></a>
+        <a href="login">Masuk</a>        
+      </nav>      
+    </header>
+    <!-- END OF HEADER -->
+    
+    <!-- MAIN CONTAINER -->
+    <section class="main-container" >
+      <div class="location" id="home">
+          <h1 id="home">Daily</h1>
+          <div class="box">
+            <a href=""><img src="../image/cover/1.jpeg" ></a>
+            <a href=""><img src="../image/cover/2.png" ></a>    
+          </div>
+      </div>
+      
+
+      <h1 id="myList">Trending</h1>
+      <div class="box">
+        <a href=""><img src="../image/cover/4.jpg" ></a>
+        <a href=""><img src="../image/cover/5.jpg" ></a>
+        <a href=""><img src="../image/cover/6.jpg" ></a>
+        <a href=""><img src="../image/cover/7.jpg" ></a>
+        <a href=""><img src="../image/cover/8.jpeg" ></a>
+        <a href=""><img src="../image/cover/9.jpg" ></a>
+        <a href=""><img src="../image/cover/10.jpg" ></a>
+
+      </div>
+      
+      <h1 id="tvShows">Sastra</h1>
+      <div class="box">
+        <a href=""><img src="../image/cover/11.jpg" ></a>
+        <a href=""><img src="../image/cover/12.jpg" ></a>  
+        <a href=""><img src="../image/cover/13.jpg" ></a>       
+        <a href=""><img src="../image/cover/14.jpg" ></a>    
+        <a href=""><img src="../image/cover/15.jpg" ></a>
+        <a href=""><img src="../image/cover/16.jpg" ></a>
+      </div>
+      
+
+      <h1 id="originals">Khusus</h1>
+      <div class="box">
+        <a href=""><img src="../image/cover/17.jpg" ></a>       
+        <a href=""><img src="../image/cover/18.jpg" ></a>         
+        <a href=""><img src="../image/cover/16.jpg" ></a>
+        <a href=""><img src="../image/cover/19.jpg" ></a>
+      </div>
+     
+    <!-- END OF MAIN CONTAINER -->
+
+    <!-- LINKS -->
+    
+    <!-- END OF LINKS -->
+
+    <!-- FOOTER -->
+    <footer>
+      <p>&copy 2021-2022 Thinksmart Bra Jendra, CV.</p>
+      <p>Intern &work 2021-2022</p>
+    </footer>
+  </div>
+</body>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+<script src="https://kit.fontawesome.com/yourcode.js" crossorigin="anonymous"></script>
+</html>
---
+++ resources/views/login.blade.php
@@ -0,0 +1,109 @@
+<html>
+<head>
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>E-Perpus</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <script defer src="https://use.fontawesome.com/releases/v5.1.0/js/all.js" integrity="sha384-3LK/3kTpDE/Pkp8gTNp2gR/2gOiwQ6QaO7Td0zV76UFJVhqLl4Vl3KL1We6q6wR9" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <link href="../css/app.css" rel="stylesheet">
+  
+</head>
+<body>
+  <div class="wrapper">
+
+    <!-- HEADER -->
+    <header>
+      <div class="netflixLogo">
+        <a id="logo" href="#home"><img src="../image/logo.png" alt="Logo Image"></a>
+      </div>      
+      <nav class="main-nav">                
+       <div class="search">
+        <input type="search" id="search" class="form-control" placeholder="Search" >
+        <button type="SUBMIT" class="btn btn-primary">
+          <i class="fas fa-search"></i>
+        </button>
+      </div>
+      </nav>
+      <nav class="sub-nav">
+        <a href="#">Bantuan</a>
+       
+        <a href="#"><i class="fas fa-bell sub-nav-logo"></i></a>
+        <a href="#">login</a>        
+      </nav>      
+    </header>
+    <!-- END OF HEADER -->
+    
+    <!-- MAIN CONTAINER -->
+    
+
+
+	<div class="section">
+		<div class="container">
+			<div class="row full-height justify-content-center">
+				<div class="col-12 text-center align-self-center py-5">
+					<div class="section pb-5 pt-5 pt-sm-2 text-center">
+					
+			          	<input class="checkbox" type="checkbox" id="reg-log" name="reg-log"/>
+			          	<label for="reg-log"></label>
+						<div class="card-3d-wrap mx-auto">
+							<div class="card-3d-wrapper">
+								<div class="card-front">
+									<div class="center-wrap">
+										<div class="section text-center">
+											<h4 class="mb-4 pb-3">Log In</h4>
+											<div class="form-group">
+												<input type="email" name="logemail" class="form-style" placeholder="Your Email" id="logemail" autocomplete="off">
+												<i class="input-icon uil uil-at"></i>
+											</div>	
+											<div class="form-group mt-2">
+												<input type="password" name="logpass" class="form-style" placeholder="Your Password" id="logpass" autocomplete="off">
+												<i class="input-icon uil uil-lock-alt"></i>
+											</div>
+											<a href="#" class="btn mt-4">submit</a>
+                            				<p class="mb-0 mt-4 text-center"><a href="#0" class="link">Forgot your password?</a></p>
+				      					</div>
+			      					</div>
+			      				</div>
+								<div class="card-back">
+									<div class="center-wrap">
+										<div class="section text-center">
+											<h4 class="mb-4 pb-3">Sign Up</h4>
+											<div class="form-group">
+												<input type="text" name="logname" class="form-style" placeholder="Your Full Name" id="logname" autocomplete="off">
+												<i class="input-icon uil uil-user"></i>
+											</div>	
+											<div class="form-group mt-2">
+												<input type="email" name="logemail" class="form-style" placeholder="Your Email" id="logemail" autocomplete="off">
+												<i class="input-icon uil uil-at"></i>
+											</div>	
+											<div class="form-group mt-2">
+												<input type="password" name="logpass" class="form-style" placeholder="Your Password" id="logpass" autocomplete="off">
+												<i class="input-icon uil uil-lock-alt"></i>
+											</div>
+											<a href="#" class="btn mt-4">submit</a>
+				      					</div>
+			      					</div>
+			      				</div>
+			      			</div>
+			      		</div>
+			      	</div>
+		      	</div>
+	      	</div>
+	    </div>
+	</div> 
+   
+      
+
+    
+    
+
+    <!-- END OF MAIN CONTAINER -->
+
+  </div>
+</body>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+<script src="https://kit.fontawesome.com/yourcode.js" crossorigin="anonymous"></script>
+</html>
